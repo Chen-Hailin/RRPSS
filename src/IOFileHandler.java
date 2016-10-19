5056f56373383b039917824b642effff0dfb9436
timothygk <timothykurniawan@gmail.com>
2016-10-20 02:45:52 +0800

    initial commit IOFileHandler to handle read/write object from/to file

---
+++ src/IOFileHandler.java
@@ -0,0 +1,73 @@
+import java.io.*;
+
+/**
+* Class to handle read from file and write to file
+*/
+class IOFileHandler {
+
+    /**
+    * return the object to read from the file which store an object
+    * to use, just do :
+    * SomeClass smth = (SomeClass) readFileObject(fileName).readObject();
+    */
+    public ObjectInputStream readFileObject (String fileName) {
+        FileInputStream fin;
+        ObjectInputStream i = null;
+
+        try {
+            fin = new FileInputStream (fileName);
+            i = new ObjectInputStream (fin);
+        } catch (FileNotFoundException nf) {
+
+        } catch (IOException io) {
+
+        }
+
+        return i;
+    }
+
+    /**
+    * return the object to write the file which will store the object data
+    * to use, just do :
+    * writeFileObject(fileName).writeObject(someObject);
+    */
+    public ObjectOutputStream writeFileObject (String fileName) {
+        FileOutputStream fout;
+        ObjectOutputStream o = null;
+
+        try {
+            fout = new FileOutputStream (fileName);
+            o = new ObjectOutputStream (fout);
+        } catch (FileNotFoundException nf) {
+
+        } catch (IOException io) {
+
+        }
+
+        return o;
+    }
+
+    /**
+    * store the object passed to the corresponding file
+    */
+    public void writeObj (String fileName, Object obj) {
+        try {
+            writeFileObject(fileName).writeObject(obj);
+        } catch (IOException io) {
+            return;
+        }
+    }
+
+    /**
+    * return the object that going to be read
+    */
+    public Object readObj (String fileName) {
+        try {
+            return readFileObject(fileName).readObject();
+        } catch (IOException io) {
+            return null;
+        } catch (ClassNotFoundException c) {
+            return null;
+        }
+    }
+}
