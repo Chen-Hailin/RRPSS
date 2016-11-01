f6218f74f5194fa2742d2d9fc760c3022f21556b
shantho <shanthhjoe.d@hotmail.com>
2016-11-01 15:52:28 +0800

    Update Rev_report.java

--- src/Rev_report.java
+++ src/Rev_report.java
@@ -17,10 +17,10 @@ public int getDate(){
 public void setDate(){
 	this.date = date;
 }
-public Promotion_set[] getSetSold(){
+public Promo_set[] getSetSold(){
 	return set_sold;
 }
-public void setSetSold(Promotion_set[] set_sold){
+public void setSetSold(Promo_Set[] set_sold){
 	this.set_sold = set_sold;
 }
 public double getRevenue(){
@@ -33,7 +33,7 @@ public void add_report(Menu_Item[] i){
 	
 }
 
-public void add_report(Promotion_set[] s){
+public void add_report(Promo_Set[] s){
 	
 }
 
