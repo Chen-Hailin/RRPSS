880089c372b3768e4b9e5d2b42447c977208351a
shantho <shanthhjoe.d@hotmail.com>
2016-11-01 16:05:32 +0800

    Update Rev_rep_list.java

--- src/Rev_rep_list.java
+++ src/Rev_rep_list.java
@@ -1,12 +1,19 @@
 
 public class Rev_rep_list {
 	private Rev_report[] reps;
-	
+	private Reservation[] rev_date;
 	public Rev_report[] getreps(){
 		return reps;
 	}
 	public void setGrades(Rev_report[] reps){
 		this.reps = reps;
+	}
+	public Reservation[] getrev_date()
+	{
+	
+        }
+	public void setReservation(Reservation[] res){
+		
 	}
 	public void print(int start, int end){
 		
