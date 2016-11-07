4b5ac60bbf7a0b873911f2386579c676c8458742
timothygk <timothykurniawan@gmail.com>
2016-11-08 00:57:09 +0800

    add hashcode and fix equals code

--- src/Reservation.java
+++ src/Reservation.java
@@ -11,7 +11,7 @@ class Reservation implements Serializable {
     private String name;
     private String contactNumber;
     private int tableID;
-    
+
     /**
     * Constructor.
     * Create a new reservation with the information provided, and set the reserved table to "reserved".
@@ -23,47 +23,62 @@ class Reservation implements Serializable {
         this.name            = name;
         this.contactNumber   = contactNumber;
         this.tableID         = tableID;
-        
-        SimpleDateFormat ft = new SimpleDateFormat("yyyyMMdd");   
+
+        SimpleDateFormat ft = new SimpleDateFormat("yyyyMMdd");
         String date = ft.format(arrivalTime);
         ft = new SimpleDateFormat("a");
         if (ft.format(arrivalTime) == "AM") date.concat("0");
         else date.concat("1");
         TablesList.getInstance().changeStatus(Integer.parseInt(date), tableID, "reserved");
     }
-    
+
     public Date getArrivalTime() {
         return arrivalTime;
     }
-    
+
     public int getNumberOfPax() {
         return numberOfPax;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public String getContactNumber() {
         return contactNumber;
     }
-    
+
     public int getTableID() {
         return tableID;
     }
-    
+
     /**
     * Indicates whether other reservation is equal to this reservation.
     */
-    public boolean equals (Reservation other) {
-        if (getArrivalTime().equals(other.getArrivalTime()) == false) return false;
-        if (getNumberOfPax()    != other.getNumberOfPax()) return false;
-        if (getName()           != other.getName()) return false;
-        if (getContactNumber()  != other.getContactNumber()) return false;
-        if (getTableID()        != other.getTableID()) return false;
-        return true;
+    public boolean equals (Object o) {
+        if (o instanceof Reservation) {
+            Reservation other = (Reservation) o;
+            if (getArrivalTime().equals(other.getArrivalTime()) == false) return false;
+            if (getNumberOfPax()    != other.getNumberOfPax()) return false;
+            if (getName().equals(other.getName()) == false) return false;
+            if (getContactNumber().equals(other.getContactNumber()) == false) return false;
+            if (getTableID()        != other.getTableID()) return false;
+            return true;
+        }
+        return false;
     }
-    
+
+    public int hashCode() {
+        SimpleDateFormat ft = new SimpleDateFormat("yyyyMMdd");
+        String date = ft.format(arrivalTime);
+        ft = new SimpleDateFormat("a");
+        if (ft.format(arrivalTime) == "AM") date.concat("0");
+        else date.concat("1");
+        return (Integer.parseInt(contactNumber) * contactNumber.length() +
+        name.hashCode() * (contactNumber.length() + name.length()) +
+        Integer.parseInt(date)) * 100 + tableID;
+    }
+
     /**
     * Check the reservation status.
     */
