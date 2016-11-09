25103f907cd2278c6b0fe4f862306c50a7ff6248
timothygk <timothykurniawan@gmail.com>
2016-11-09 19:25:22 +0800

    add maxID variable and modify print

--- src/StaffList.java
+++ src/StaffList.java
@@ -7,11 +7,12 @@ import java.io.Serializable;
 */
 public class StaffList implements Serializable {
     private Map<Integer, Staff> staffList = new HashMap<Integer, Staff>();;
-        
-    private static StaffList instance = null;    
-    
+    private int maxID = 1;
+
+    private static StaffList instance = null;
+
     private StaffList(){};
-    
+
     /**
     * App can use this to get singleton reference
     */
@@ -24,7 +25,7 @@ public class StaffList implements Serializable {
 	public void setInstance() {
 		instance = this;
 	}
-    
+
     /**
     * Add a new staff to the list.
     * @param id The new if of the staff.
@@ -33,13 +34,11 @@ public class StaffList implements Serializable {
     * @param jobTitle The new job title of the staff.
     * @return False if id already exist. True if the staff has been added to the list successfully.
     */
-    public boolean addStaff(int id, String name, String gender, String jobTitle) {
-        if (staffList.containsKey(new Integer(id))) 
-            return false;
-        staffList.put(new Integer(id), new Staff(id, name, gender, jobTitle));
-        return true;
+    public void addStaff(String name, String gender, String jobTitle) {
+        staffList.put(new Integer(maxID), new Staff(maxID, name, gender, jobTitle));
+        maxID++;
     }
-    
+
     /**
     * Removes a staff based on the id.
     * @param id The employee ID of the staff.
@@ -48,7 +47,7 @@ public class StaffList implements Serializable {
     public Staff removeStaff(int id) {
         return staffList.remove(new Integer(id));
     }
-    
+
     /**
     * Get a staff object based on his id.
     * @param id The employee ID of the staff.
@@ -57,13 +56,14 @@ public class StaffList implements Serializable {
     public Staff getStaff(int id) {
         return staffList.get(new Integer(id));
     }
-    
+
     /**
     * Print all available staff information.
     */
     public void printAll() {
         for (Map.Entry<Integer, Staff> entry: staffList.entrySet()) {
             entry.getValue().print();
+            System.out.println ("####################");
         }
     }
 };
