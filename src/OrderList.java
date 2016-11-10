09ceaa9d3f9ddeefeb49bba7c3a8f36947b40de1
Prabowo <Prabowo>
2016-11-10 14:58:15 +0800

    add javadoc

--- src/OrderList.java
+++ src/OrderList.java
@@ -2,23 +2,36 @@ import java.util.*;
 import java.text.SimpleDateFormat;
 import java.io.Serializable;
 
-class OrderList implements Serializable {
+/**
+* Stores the list of orders
+*/
+public class OrderList implements Serializable {
     private Map<Reservation, Order> orderList;
 
     private static OrderList instance = null;
 
+	//Exist only to defeat instantiation
     private OrderList(){};
 
+    /**
+    * APP can use this to get the singleton reference
+    */
     public static OrderList getInstance(){
         if(instance == null)
             instance = new OrderList();
         return instance;
     }
 
+	/**
+	* Used to update the instance.
+	*/
     public void setInstance() {
         instance = this;
     }
 
+	/**
+	* @return A hash map which map a reservation to an order.
+	*/
     public Map<Reservation, Order> getOrderList() {
         if (orderList == null)
             orderList = new HashMap<Reservation, Order> ();
@@ -26,14 +39,26 @@ class OrderList implements Serializable {
         return orderList;
     }
 
+	/**
+	* Get an order using reservation as the key.
+	* @param key The key to identify the order.
+	* @return The queried order.
+	*/
     public Order getOrder(Reservation key) {
         return getOrderList().get(key);
     }
 
+	/**
+	* Remove an order from the list.
+	* @param reservation The reservation which made the order.
+	*/
     public void removeOrder (Reservation reservation) {
         orderList.remove(reservation);
     }
 
+	/**
+	* Print all available orders.
+	*/
     public void printOrder() {
         for (Map.Entry <Reservation, Order> entry : getOrderList().entrySet()) {
             entry.getValue().print(Menu.getInstance(), PromoSet.getInstance());
